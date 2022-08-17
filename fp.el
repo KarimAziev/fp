819b684398743f4acd7b3134ca47c1102255b172
Karim Aziiev <karim.aziiev@gmail.com>
2022-08-17 15:49:05 +0300

    0.2.0

--- fp.el
+++ fp.el
@@ -5,7 +5,7 @@
 ;; Author: Karim Aziiev <karim.aziiev@gmail.com>
 ;; URL: https://github.com/KarimAziev/fp
 ;; Keywords: lisp
-;; Version: 0.1.2
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This file is NOT part of GNU Emacs.

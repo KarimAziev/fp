cec12582abf506734583bf4213dec5d6f01de7d6
Karim Aziiev <karim.aziiev@gmail.com>
2022-08-19 13:12:47 +0300

    v1.0.0

--- fp.el
+++ fp.el
@@ -5,8 +5,8 @@
 ;; Author: Karim Aziiev <karim.aziiev@gmail.com>
 ;; URL: https://github.com/KarimAziev/fp
 ;; Keywords: lisp
-;; Version: 0.3.0
-;; Package-Requires: ((emacs "27.1"))
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "25.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -32,7 +32,7 @@
 ;;; Code:
 
 ;;;###autoload
-(defmacro fp--pipe (&rest functions)
+(defmacro fp-pipe (&rest functions)
   "Return left-to-right composition from FUNCTIONS."
   (declare (debug t) (pure t) (side-effect-free t))
   `(lambda (&rest args)
@@ -49,13 +49,13 @@
              `(apply ,init-fn args)))))))
 
 ;;;###autoload
-(defmacro fp--compose (&rest functions)
+(defmacro fp-compose (&rest functions)
   "Return right-to-left composition from FUNCTIONS."
   (declare (debug t) (pure t) (side-effect-free t))
-  `(fp--pipe ,@(reverse functions)))
+  `(fp-pipe ,@(reverse functions)))
 
 ;;;###autoload
-(defmacro fp--or (&rest functions)
+(defmacro fp-or (&rest functions)
   "Return an unary function which invoke FUNCTIONS until first non-nil result."
   (declare (debug t) (pure t) (side-effect-free t))
   `(lambda (it) (or
@@ -65,7 +65,7 @@
                       functions))))
 
 ;;;###autoload
-(defmacro fp--and (&rest functions)
+(defmacro fp-and (&rest functions)
   "Return an unary function which invoke FUNCTIONS until first nil result."
   (declare (debug t) (pure t) (side-effect-free t))
   `(lambda (it) (and
@@ -75,7 +75,7 @@
                       functions))))
 
 ;;;###autoload
-(defmacro fp--partial (fn &rest args)
+(defmacro fp-partial (fn &rest args)
   "Return a partial application of FN to left-hand ARGS.
 
 ARGS is a list of the last N arguments to pass to FN. The result is a new
@@ -88,7 +88,7 @@ at the values with which this function was called."
                    `(apply ,fn (append (list ,@args) pre-args)))))))
 
 ;;;###autoload
-(defmacro fp--rpartial (fn &rest args)
+(defmacro fp-rpartial (fn &rest args)
   "Return a partial application of FN to right-hand ARGS.
 
 ARGS is a list of the last N arguments to pass to FN. The result is a new
@@ -101,14 +101,14 @@ at the values with which this function was called."
                    `(apply ,fn (append pre-args (list ,@args))))))))
 
 ;;;###autoload
-(defmacro fp--converge (combine-fn &rest functions)
+(defmacro fp-converge (combine-fn &rest functions)
   "Return a function that apply COMBINE-FN with results of branching FUNCTIONS.
 If first element of FUNCTIONS is vector, it will be used instead.
 
 Example:
 
-\(funcall (fp--converge concat [upcase downcase]) \"John\").
-\(funcall (fp--converge concat upcase downcase) \"John\")
+\(funcall (fp-converge concat [upcase downcase]) \"John\").
+\(funcall (fp-converge concat upcase downcase) \"John\")
 
 Result: \"JOHNjohn\"."
   `(lambda (&rest args) (apply
@@ -126,7 +126,7 @@ Result: \"JOHNjohn\"."
                                  functions))))))
 
 ;;;###autoload
-(defmacro fp--when (pred fn)
+(defmacro fp-when (pred fn)
   "Return an unary function that invoke FN if result of calling PRED is non-nil.
 Both PRED and FN called with one argument.
 If result of PRED is nil, return the argument as is."
@@ -139,7 +139,7 @@ If result of PRED is nil, return the argument as is."
               arg)))
 
 ;;;###autoload
-(defmacro fp--unless (pred fn)
+(defmacro fp-unless (pred fn)
   "Return an unary function that invoke FN if result of calling PRED is nil.
 Both PRED and FN called with one argument.
 If result of PRED is non nil return the argument as is."
@@ -152,7 +152,7 @@ If result of PRED is non nil return the argument as is."
                  `(funcall ,fn arg)))))
 
 ;;;###autoload
-(defmacro fp--const (value)
+(defmacro fp-const (value)
   "Return a function that always return VALUE.
 
 This function accepts any number of arguments, but ignores them."
@@ -160,63 +160,13 @@ This function accepts any number of arguments, but ignores them."
   `(lambda (&rest _) ,value))
 
 ;;;###autoload
-(defmacro fp--ignore-args (fn)
+(defmacro fp-ignore-args (fn)
   "Return a function that invoke FN without args.
 
 This function accepts any number of arguments, but ignores them."
   `(lambda (&rest _) ,(if (symbolp fn)
                      `(,fn)
                    `(funcall ,fn arg))))
-
-;;;###autoload
-(defun fp-partial (fn &rest args)
-  "Return a function that is a partial application of FN to ARGS.
-
-ARGS is a list of the first N arguments to pass to FN.
-The result is a new function which does the same as FN, except that
-the first N arguments are fixed at the values with which this function
-was called."
-  (lambda (&rest args2)
-    (apply fn (append args args2))))
-
-;;;###autoload
-(defun fp-rpartial (fn &rest args)
-  "Return a partial application of FN to right-hand ARGS.
-
-ARGS is a list of the last N arguments to pass to FN. The result is a new
-function which does the same as FN, except that the last N arguments are fixed
-at the values with which this function was called."
-  (lambda (&rest pre-args)
-    (apply fn (append pre-args args))))
-
-;;;###autoload
-(defun fp-compose (&rest functions)
-  "Return right-to-left composition from FUNCTIONS."
-  (lambda (&rest args)
-    (car (seq-reduce (lambda (xs fn) (list (apply fn xs)))
-                     (reverse functions) args))))
-
-;;;###autoload
-(defun fp-pipe (&rest functions)
-  "Return right-to-left composition from FUNCTIONS."
-  (lambda (&rest args)
-    (car (seq-reduce (lambda (xs fn) (list (apply fn xs)))
-                     functions args))))
-
-;;;###autoload
-(defun fp-compose-while-not-nil (&rest functions)
-  "Return right-to-left composition from FUNCTIONS."
-  (let ((fn))
-    (setq functions (reverse functions))
-    (setq fn (pop functions))
-    (lambda (&rest args)
-      (let ((arg (unless (null (flatten-list args))
-                   (apply fn args))))
-        (while (setq fn (unless (null arg)
-                          (pop functions)))
-          (let ((res (apply fn (list arg))))
-            (setq arg res)))
-        arg))))
 
 (provide 'fp)
 ;;; fp.el ends here
